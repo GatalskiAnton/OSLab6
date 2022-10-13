ae6c864d7c1e434e391670dc2b7be80acb2034a3
AntonGatalski <ag629331@gmail.com>
2022-10-13 14:38:13 +0300

    added mutex

--- OSLab4/main.cpp
+++ OSLab4/main.cpp
@@ -3,16 +3,21 @@
 #include <fstream>
 #include <sstream>
 #include <thread>
+#include <mutex>
+
 
 std::vector<double> threadResults;
 std::vector<double> seqResults;
+
+std::mutex g_lock;
 
 void blocksMul(const std::vector <std::vector <int>>&m1, const std::vector <std::vector <int>>&m2, std::vector < std::vector <int>>&resM, int blockI, int blockJ, int matrixSize, int blockSize)
 {
 	for (int i = blockI; i < std::min(blockI + blockSize,matrixSize); ++i)
 		for (int j = blockJ; j < std::min(blockJ + blockSize,matrixSize); ++j)
-			for(int k = 0; k < matrixSize; ++k)
+			for (int k = 0; k < matrixSize; ++k)
 				resM[i][j] += m1[i][k] * m2[k][j];
+			
 }
 
 void defaultMul(const std::vector <std::vector <int>>& m1, const std::vector <std::vector <int>>& m2, std::vector < std::vector <int>>& resM, int matrixSize, int blockSize)
@@ -27,9 +32,13 @@ void threadMul(const std::vector <std::vector <int>>& m1, const std::vector <std
 	std::vector<std::thread> threads;
 	for (int blockI = 0; blockI < matrixSize; blockI += blockSize)
 		for (int blockJ = 0; blockJ < matrixSize; blockJ += blockSize)
+		{
+			g_lock.lock();
 			threads.emplace_back(blocksMul, std::cref(m1), std::cref(m2), std::ref(resM), blockI, blockJ, matrixSize, blockSize);
-	for (auto& thrd: threads)
-		thrd.join();
+			g_lock.unlock();
+		}
+		for (auto& thrd: threads)
+			thrd.join();
 }
 
 void getMatrix(std::vector< std::vector<int>> &m1, std::string file)
